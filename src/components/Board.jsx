69d8fd960643d80a0a60242fb537006ae3f4b4cf
Shivdeep Singh <sshivdeeprapyal@gmail.com>
2023-10-25 16:21:53 +0530

    added board, squares, scss, buttons

---
+++ src/components/Board.jsx
@@ -0,0 +1,27 @@
+
+import Square from './Square'
+const Board = () => {
+  return (
+    <div className='board'>
+        <div className='board-row'>
+            <Square value={0}></Square>
+            <Square value={1}></Square>
+            <Square value={2}></Square>
+        </div>
+        <div className='board-row'>
+            <Square value={3}></Square>
+            <Square value={4}></Square>
+            <Square value={5}></Square>
+        </div>
+        <div className='board-row'>
+            <Square value={6}></Square>
+            <Square value={7}></Square>
+            <Square value={8}></Square>
+        </div>        
+        
+
+    </div>
+  )
+}
+
+export default Board
